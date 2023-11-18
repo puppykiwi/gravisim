b9ca75469cbc825d1672ef70f34f9c3bf1a44aaf
puppykiwi <johnraymwendwa04@gmail.com>
2023-11-18 15:02:38 +0300

    .

--- planet.py
+++ planet.py
@@ -3,11 +3,9 @@ import pygame
 from util import *
 
 class Planet:
-    def __init__(self, x, y, mass, display, planet):
+    def __init__(self, x, y, display):
         self.x = x
         self.y = y
-        self.mass = mass
         self.display = display
-        self.planet = planet
     def draw(self):
-        self.display.blit(self.planet, (self.x - PLANET_SIZE, self.y - PLANET_SIZE))
+        self.display.blit(PLANET, (self.x - PLANET_SIZE, self.y - PLANET_SIZE))
